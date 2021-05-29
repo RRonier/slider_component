abccf4cf38744040f7f041738f77b46f7afeb9c0
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-05-29 02:01:03 -0400

    cleaning slides.jsx

--- client/src/SliderComponent/Slider.jsx
+++ client/src/SliderComponent/Slider.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
-import Card from "./Card";
 import styled from "styled-components";
+import { Box } from "grommet";
+import { IconButton } from "grommet-controls";
+import { FormNext, FormPrevious } from "grommet-icons";
 
-const Slider = () => {
+
+const Slider = ({title, ...props}) => {
   // const [currentCard, setCurrentCard] = useState(null);
   let card_container;
   useEffect(() => {
@@ -10,29 +13,43 @@ const Slider = () => {
   });
 
   return (
-    <Div ref={(ref_id) => (card_container = ref_id)}>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </Div>
+    <Box
+      direction="column"
+      border="all"
+      justify="center"
+      background={{ color: "black" }}
+    >
+      <StyledDiv>
+        <div>
+          <Title>{title}</Title>
+        </div>
+        <ButtonContainer>
+          <IconButton icon={<FormPrevious />} />
+          <IconButton icon={<FormNext />} />
+        </ButtonContainer>
+      </StyledDiv>
+      <Div ref={(ref_id) => (card_container = ref_id)}>{props.children}</Div>
+    </Box>
   );
 };
 
 const Div = styled.div`
   min-width: 100%;
   display: flex;
+  overflow: hidden;
 `;
-
+const StyledDiv = styled.div`
+  min-width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const ButtonContainer = styled.div`
+  margin: 10px;
+  @media all and (min-width: 360px) {
+    display: flex;
+  }
+`;
+const Title = styled.h1`
+  margin-left: 24px;
+`;
 export default Slider;
