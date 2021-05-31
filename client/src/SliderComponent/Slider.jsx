44e92dec077a999a59cb5022b61446dd8287ce35
Ronier Ramos Sanchez <ronier2332@gmail.com>
2021-05-31 10:09:02 -0400

    almost done, find distance issue

--- client/src/SliderComponent/Slider.jsx
+++ client/src/SliderComponent/Slider.jsx
@@ -1,73 +1,101 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 import { Box } from "grommet";
 import { IconButton } from "grommet-controls";
 import { Next, Previous } from "grommet-icons";
 
 const Slider = ({ title, stepWidth, ...props }) => {
+  let card_container = useRef();
+  let box = useRef();
   const [state, setState] = useState({
-    currentCard: 0,
+    move: 0,
     box_position: 0,
+    distance: 0,
     last_card_position: 0,
-    numberOfCards: 0,
-    card_container: null,
-    lastCard: 0,
-    box: null,
+    wider: false,
   });
 
   useEffect(() => {
-    setState({
-      ...state,
-      numberOfCards: state.card_container.children.length,
-      lastCard:
-        state.card_container.children[state.card_container.children.length - 1],
-      box_position: state.box.getBoundingClientRect().right,
-      last_card_position: state.card_container.getBoundingClientRect().right,
-    });
-  }, []);
+    let distance = state.last_card_position - state.box_position;
+    setState((myState) => ({
+      ...myState,
+      box_position: box.current.getBoundingClientRect().right,
+      last_card_position: card_container.current.getBoundingClientRect().right,
+      distance: distance,
+    }));
+    if (state.last_card_position > state.box_position) {
+      setState((prevState) => ({
+        ...prevState,
+        wider: true,
+      }));
+    }
+    // if(state.distance <= 0) {
+    //   card_container.current.left = `translate(700px)`
+    // }
+  }, [state.box_position, state.last_card_position, state.distance]);
 
+  //move the cards after the button is clicked
   useEffect(() => {
-    state.card_container.style.transitionDuration = "0.5s";
-    state.card_container.style.transform = `translate(-${
-      stepWidth * state.currentCard
-    }px)`;
-  }, [state.currentCard]);
+    setState((myState) => ({
+      ...myState,
+      distance: myState.distance,
+    }));
+      card_container.current.style.transitionDuration = "0.5s";
+      card_container.current.style.transform = `translate(-${
+        stepWidth * state.move
+      }px)`;
+  }, [state.move]);
 
   let movePrevCard = () => {
-    if (state.currentCard > 0) {
-      let new_current_card = state.currentCard - 1;
-      setState({
-        ...state,
-        currentCard: new_current_card,
-      });
+    console.log(state.distance);
+    setState((prevState) => ({
+      ...prevState,
+      box_position: prevState.box_position + 2,
+      last_card_position: prevState.last_card_position + 3,
+    }));
+    setState((myState) => ({
+      ...myState,
+      box_position: box.current.getBoundingClientRect().right,
+      last_card_position: card_container.current.getBoundingClientRect().right,
+    }));
+    if (state.move > 0) {
+      let new_current_card = state.move - 1;
+      setState((prevState) => ({
+        ...prevState,
+        move: new_current_card,
+      }));
     }
     return;
   };
 
   let moveNextCard = () => {
-    console.log(`box-right: ${state.box.getBoundingClientRect().right}`);
-    console.log(
-      `lastCard-right: ${state.lastCard.getBoundingClientRect().right}`
-    );
-    console.log(
-      `cardContainer-right: ${
-        state.card_container.getBoundingClientRect().right
-      }`
-    );
-    state.lastCard.style.border = "dashed red";
-    if (state.currentCard < state.card_container.children.length - 1) {
-      let new_current_card = state.currentCard + 1;
-      setState({
-        ...state,
-        currentCard: new_current_card,
-      });
+    console.log(state.distance);
+    setState((prevState) => ({
+      ...prevState,
+      box_position: box.current.getBoundingClientRect().right,
+      last_card_position: card_container.current.getBoundingClientRect().right,
+    }));
+
+    if (state.distance <= 0) {
+      card_container.current.style.transform = `translate(${
+        box.current.getBoundingClientRect().right
+      }px)`;
+    }
+
+    // if (state.move < card_container.current.children.length - 1) {
+    if (state.last_card_position > state.box_position) {
+      let new_current_card = state.move + 1;
+      setState((prevState) => ({
+        ...prevState,
+        move: new_current_card,
+      }));
     }
     return;
   };
 
   return (
     <Box
-      ref={(ref_id) => (state.box = ref_id)}
+      ref={box}
       direction="column"
       border="all"
       background={{ color: "black" }}
@@ -80,24 +108,22 @@ const Slider = ({ title, stepWidth, ...props }) => {
           <Title>{title}</Title>
         </div>
 
-        {state.box_position <= state.last_card_position && (
+        {state.wider && (
           <ButtonContainer>
             <IconButton
               icon={<Previous />}
-              disabled={state.currentCard === 0}
+              disabled={state.move === 0}
               onClick={movePrevCard}
             />
             <IconButton
               icon={<Next />}
-              disabled={state.box_position >= state.last_card_position}
+              disabled={state.distance <= 0}
               onClick={moveNextCard}
             />
           </ButtonContainer>
         )}
       </StyledDiv>
-      <Div ref={(ref_id) => (state.card_container = ref_id)}>
-        {props.children}
-      </Div>
+      <Div ref={card_container}>{props.children}</Div>
     </Box>
   );
 };
